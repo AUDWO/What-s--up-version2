b3c34350110d38fef3ef8b702e111ad7080e4b88
audwo <kmjstj3@naver.com>
2024-02-16 12:04:27 +0900

    Create:icons

---
+++ src/components/icons/MainIcon.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { BsFillEmojiSmileFill } from "react-icons/bs";
+
+const MainIcon = styled(BsFillEmojiSmileFill)`
+  font-size: 50px;
+  color: #f7dd07;
+  border-radius: 50%;
+  background-color: #4099ff;
+`;
+
+export default MainIcon;
