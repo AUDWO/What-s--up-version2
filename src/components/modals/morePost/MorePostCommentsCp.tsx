2e53e69d26fe4c0e066b197369cef2d468072b65
audwo <kmjstj3@naver.com>
2024-02-24 22:35:17 +0900

    Create:add noComment text

--- src/components/modals/morePost/MorePostCommentsCp.tsx
+++ src/components/modals/morePost/MorePostCommentsCp.tsx
@@ -9,6 +9,11 @@ const MorePostCommentsCp = () => {
       <CommentRCp />
       <CommentRCp />
       <CommentRCp />
+      {false && (
+        <NoCommentWrapper>
+          <NoCommentText>아직 댓글이 존재하지 않습니다.</NoCommentText>
+        </NoCommentWrapper>
+      )}
     </MorePostCommentsContainer>
   );
 };
@@ -18,10 +23,22 @@ export default MorePostCommentsCp;
 const MorePostCommentsContainer = styled.div`
   align-items: center;
   padding-left: 20px;
+  padding-right: 10px;
   padding-bottom: 20px;
   overflow: auto;
   height: 360px;
   ::-webkit-scrollbar {
     display: none;
   }
+  background-color: ${(props) => props.theme.bgColor};
 `;
+
+const NoCommentWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NoCommentText = styled.div``;
